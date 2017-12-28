6cb6cece405fab3b86e03b51aba95ee5039e435a
Ville Peurala <ville.peurala@gmail.com>
2017-12-28 18:15:40 +0200

    Create target 'Aurinko'. Add .editorconfig.

---
+++ BUILD
@@ -0,0 +1,5 @@
+java_binary(
+    name = "Aurinko",
+    srcs = ["src/main/java/org/aurinkopg/Aurinko.java"],
+    main_class = "org.aurinkopg.Aurinko"
+)
---
+++ src/main/java/org/aurinkopg/Aurinko.java
@@ -0,0 +1,7 @@
+package org.aurinkopg;
+
+public class Aurinko {
+	public static void main(String[] args) throws Exception {
+               System.out.println("Aurinko!");	
+	}
+}
