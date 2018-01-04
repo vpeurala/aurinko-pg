350d3d795fe6782bcf4bd23e4fbbb546ce9e6906
Ville Peurala <ville.peurala@gmail.com>
2018-01-05 00:03:19 +0200

    Successful login.

--- src/test/java/org/aurinkopg/AurinkoTest.java
+++ src/test/java/org/aurinkopg/AurinkoTest.java
@@ -1,14 +1,14 @@
 package org.aurinkopg;
 
 import org.junit.Test;
+import org.postgresql.PGProperty;
 import org.postgresql.jdbc.PgConnection;
 import org.postgresql.util.HostSpec;
 
 import java.sql.SQLException;
 import java.util.Properties;
 
-import static org.aurinkopg.TestData.POSTGRES_DATABASE;
-import static org.aurinkopg.TestData.POSTGRES_USERNAME;
+import static org.aurinkopg.TestData.*;
 import static org.junit.Assert.assertEquals;
 
 public class AurinkoTest {
@@ -25,6 +25,8 @@ public class AurinkoTest {
     public PgConnection connect() throws SQLException {
         HostSpec hostSpec = new HostSpec("localhost", 5432);
         HostSpec[] hostSpecs = new HostSpec[]{hostSpec};
-        return new PgConnection(hostSpecs, POSTGRES_USERNAME, POSTGRES_DATABASE, new Properties(), "");
+        Properties info = new Properties();
+        PGProperty.PASSWORD.set(info, POSTGRES_PASSWORD);
+        return new PgConnection(hostSpecs, POSTGRES_USERNAME, POSTGRES_DATABASE, info, "");
     }
 }
