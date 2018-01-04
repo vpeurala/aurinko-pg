cc6c31ab9624b825ef5038e648236dafff782d5f
Ville Peurala <ville.peurala@gmail.com>
2018-01-04 23:10:42 +0200

    Swhich from Bazel to Gradle. Delete all Bazel stuff.

---
+++ src/test/java/org/aurinkopg/OtherTest.java
@@ -0,0 +1,12 @@
+package org.aurinkopg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OtherTest {
+    @Test
+    public void otherTest() {
+        assertEquals(2, 3);
+    }
+}
