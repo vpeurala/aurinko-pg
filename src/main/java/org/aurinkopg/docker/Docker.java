b622e9ba5c5eb65d226cb652ab0cb4a8960b5784
Ville Peurala <ville.peurala@gmail.com>
2018-01-07 17:28:32 +0200

    Test for case where Docker is not found.

--- src/main/java/org/aurinkopg/docker/Docker.java
+++ src/main/java/org/aurinkopg/docker/Docker.java
@@ -1,11 +1,25 @@
 package org.aurinkopg.docker;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Docker {
+    private final Map<String, String> changedEnvironmentVariables;
+
+    public Docker() {
+        this.changedEnvironmentVariables = Collections.emptyMap();
+    }
+
+    public Docker(Map<String, String> changedEnvironmentVariables) {
+        this.changedEnvironmentVariables = changedEnvironmentVariables;
+    }
+
     public boolean isDockerInstalledAndFoundOnPath() throws IOException, InterruptedException {
-        Process whichDocker = new ProcessBuilder().
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.environment().putAll(changedEnvironmentVariables);
+        Process whichDocker = processBuilder.
             inheritIO().
             command("which", "docker").
             start();
--- src/test/java/org/aurinkopg/docker/DockerTest.java
+++ src/test/java/org/aurinkopg/docker/DockerTest.java
@@ -2,11 +2,26 @@ package org.aurinkopg.docker;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class DockerTest {
     @Test
-    public void smoke() throws Exception {
-        assertEquals(true, new Docker().isDockerInstalledAndFoundOnPath());
+    public void dockerIsFoundAsItShouldBe() throws Exception {
+        assertEquals(
+            "Docker not found on path in a test where it should be found.\n" +
+                "It seems like you actually don't have Docker installed, or it is not on your PATH.\n" +
+                "Install it now from 'https://store.docker.com/search?type=edition&offering=community' before proceeding.\n",
+            true,
+            new Docker().isDockerInstalledAndFoundOnPath());
+    }
+
+    @Test
+    public void dockerIsNotFound() throws Exception {
+        Map<String, String> environment = new HashMap<>();
+        environment.put("PATH", "");
+        assertEquals(false, new Docker(environment).isDockerInstalledAndFoundOnPath());
     }
 }
