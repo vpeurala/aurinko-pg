e5b68b88690256852b9745519b2e1438cd1f932b
Ville Peurala <ville.peurala@gmail.com>
2018-01-08 16:28:34 +0200

    Better instructions in run.sh script.

--- docker/run.sh
+++ docker/run.sh
@@ -7,11 +7,11 @@ echo "Container status: $CONTAINER_STATUS";
 if [[ "$CONTAINER_STATUS" == "jaanmurtaja-db: Up"* ]]; then
   echo "The container is already running.";
   if [[ "$1" == "-f" || "$1" == "--force" ]]; then
-    echo "You used option \"$1\", so we destroy the current container and start a new one.";
+    echo "You used the \"force\" option \"$1\", so we destroy the current container and start a new one.";
     docker rm -f jaanmurtaja-db;
     echo "Old container destroyed, starting a new one...";
   else
-    echo "Run with the --force (-f) option if you want to destroy the current container and start a new one.";
+    echo "Run with the \"force\" (-f, --force) option if you want to destroy the current container and start a new one.";
     echo "Like this: \"$0 --force\".";
     echo "Exiting now and leaving the existing container running.";
     exit 1;
