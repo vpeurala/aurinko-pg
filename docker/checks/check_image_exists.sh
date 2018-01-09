be18d87bc7a7bbd0169779cca98bf80e44a31333
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 02:34:46 +0200

    New image name in check_image_exists.sh.

--- docker/checks/check_image_exists.sh
+++ docker/checks/check_image_exists.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker images --filter="reference=aurinko/postgresql-9.5.5:latest" --format="{{.Repository}}:{{.Tag}}" --no-trunc;
+docker images --filter="reference=vpeurala/aurinko-pg-9.5.5:latest" --format="{{.Repository}}:{{.Tag}}" --no-trunc;
