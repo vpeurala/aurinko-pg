667ce9f92a38f2460968595e95514d1ff154bec1
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 12:05:01 +0200

    This is version 0.4 of the Java code, so change version from 0.4-SNAPSHOT to 0.4. Tag later, when gradle config etc. are ok.

--- src/main/java/org/aurinkopg/GlobalConstants.java
+++ src/main/java/org/aurinkopg/GlobalConstants.java
@@ -2,5 +2,5 @@ package org.aurinkopg;
 
 public class GlobalConstants {
     public static final String LIBRARY_NAME = "Aurinko";
-    public static final String LIBRARY_VERSION = "0.4-SNAPSHOT";
+    public static final String LIBRARY_VERSION = "0.4";
 }
