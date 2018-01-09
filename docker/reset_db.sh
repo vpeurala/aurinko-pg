8df4a16d7bd2a14913bdef1cef3d9b80e86a7844
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 16:39:55 +0200

    Script for resetting the database. Has not proven to be very useful.

--- docker/reset_db.sh
+++ docker/reset_db.sh
@@ -4,5 +4,6 @@ pushd . >/dev/null;
 cd ${PROJECT_ROOT}/docker;
 PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < drop.sql
 PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < init.sql
+echo "UPDATE pg_database SET datallowconn = 'true'" | PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja;
 popd >/dev/null;
 
