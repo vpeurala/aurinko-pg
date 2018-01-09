f8ea75a74bb50568ee135f8c078539ae716d55a1
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 02:41:06 +0200

    Change the image name also in TestFixtures.

--- src/test/java/org/aurinkopg/TestFixtures.java
+++ src/test/java/org/aurinkopg/TestFixtures.java
@@ -8,7 +8,7 @@ public class TestFixtures {
     public static final String POSTGRES_USERNAME = "jaanmurtaja";
     public static final String POSTGRES_PASSWORD = "argxBX4DxWJKC7st";
     public static final String POSTGRES_DATABASE = "jaanmurtaja";
-    public static final String TEST_DOCKER_IMAGE_NAME = "aurinko/postgresql-9.5.5:latest";
+    public static final String TEST_DOCKER_IMAGE_NAME = "vpeurala/aurinko-pg-9.5.5:latest";
     public static final String TEST_DOCKER_CONTAINER_NAME = "jaanmurtaja-db";
 
     public static final ConnectionInfo.Builder CONNECTION_INFO_BUILDER_WHICH_CONNECTS_TO_TEST_DOCKER_CONTAINER =
