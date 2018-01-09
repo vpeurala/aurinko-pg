af3193d2808d30e65dc501ad628e8bd1fbec6c95
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 14:46:47 +0200

    Make reset_db.sh runnable from anywhere.

--- docker/reset_db.sh
+++ docker/reset_db.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+PROJECT_ROOT=$(git rev-parse --show-toplevel);
+pushd . >/dev/null;
+cd ${PROJECT_ROOT}/docker;
 PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < drop.sql
 PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < init.sql
+popd >/dev/null;
 
