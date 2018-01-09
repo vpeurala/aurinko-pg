c729e5048dfbc79478cd067d557ea19862ce98d5
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 22:32:43 +0200

    Useful deploy_local.sh script for testing aurinko-pg in another project.

---
+++ deploy_local.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./docker/build.sh && ./docker/run.sh --force && gradle build && gradle assemble && cp build/libs/aurinko-pg-0.4.jar $HOME/Projects/Wunderdog/Arctech/arctech4/lib/aurinko-pg-0.4.jar && docker rm -f jaanmurtaja-db;
