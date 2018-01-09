e77984d61473ddd7131ddf31ad4765734056a054
Ville Peurala <ville.peurala@gmail.com>
2018-01-09 02:22:30 +0200

    Use tag 'vpeurala/aurinko-pg-9.5.5' instead of 'aurinko/postgresql-9.5.5' for images of this project.

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,5 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel);
 pushd .;
 cd ${PROJECT_ROOT}/docker;
-docker build --file Dockerfile --tag aurinko/postgresql-9.5.5:latest .;
+docker build --file Dockerfile --tag vpeurala/aurinko-pg-9.5.5:latest .;
 popd;
-
