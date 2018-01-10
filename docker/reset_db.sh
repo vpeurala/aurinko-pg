9cc1c0d2a44cdd8dbc0ded7c99681860922e00b8
Ville Peurala <ville.peurala@gmail.com>
2018-01-10 17:43:51 +0200

    Delete unnecessary script.

--- docker/reset_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-PROJECT_ROOT=$(git rev-parse --show-toplevel);
-pushd . >/dev/null;
-cd ${PROJECT_ROOT}/docker;
-PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < drop.sql
-PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja < init.sql
-echo "UPDATE pg_database SET datallowconn = 'true'" | PGPASSWORD=argxBX4DxWJKC7st psql -h "0.0.0.0" -p 5432 -U jaanmurtaja jaanmurtaja;
-popd >/dev/null;
-
