6b631c94a7868262e1647748a4d6f07adc4cb314
Ville Peurala <ville.peurala@gmail.com>
2018-01-14 02:54:08 +0200

    Start testing with dbcp2.

---
+++ src/test/java/org/aurinkopg/integrationtests/Dbcp2IntegrationTest.java
@@ -0,0 +1,13 @@
+package org.aurinkopg.integrationtests;
+
+import org.junit.Test;
+
+/**
+ * ScalikeJDBC v.3.1.0 used dbcp2 as a connection pool by default.
+ */
+public class Dbcp2IntegrationTest {
+    @Test
+    public void smoke() throws Exception {
+        // TODO Implement
+    }
+}
