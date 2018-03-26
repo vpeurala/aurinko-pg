ca42e52fb52bf02b1bb0ccb00b7607af45dba288
Ville Peurala <ville.peurala@wunderdog.fi>
2018-03-26 09:32:05 +0300

    Complete BoilerplateTest for now

--- src/test/java/org/aurinkopg/coverage/BoilerplateTest.java
+++ src/test/java/org/aurinkopg/coverage/BoilerplateTest.java
@@ -1,7 +1,9 @@
 package org.aurinkopg.coverage;
 
+import org.aurinkopg.postgresql.ConnectionFactory;
 import org.aurinkopg.postgresql.ConnectionInfo;
 import org.junit.Test;
+import org.postgresql.PGProperty;
 
 import java.util.HashMap;
 
@@ -10,5 +12,15 @@ public class BoilerplateTest {
     public void invokeToString() throws Exception {
         new ConnectionInfo("", 0, "", "", "", new HashMap<>())
             .toString();
+    }
+
+    @Test
+    public void invokeAddConnectionProperty() throws Exception {
+        new ConnectionInfo.Builder().addConnectionProperty(PGProperty.APPLICATION_NAME, "Aurinko");
+    }
+
+    @Test
+    public void invokeConstructorOfConnectionFactory() throws Exception {
+        new ConnectionFactory();
     }
 }
