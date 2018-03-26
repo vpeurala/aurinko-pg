9cb36243333f131f1f425a8294e99783c2ba1a12
Ville Peurala <ville.peurala@wunderdog.fi>
2018-03-26 09:45:35 +0300

    Delete unused class

--- src/main/java/org/aurinkopg/os/ShellCommands.java
+++
@@ -1,9 +0,0 @@
-package org.aurinkopg.os;
-
-public class ShellCommands {
-    // Get the real name of the logged in user:
-    // finger -l | head -n 1 | sed 's/^.*Name: //g' | tr -d '\n'
-
-    // Get the username of the logged in user:
-    // finger -l | head -n 1 | sed 's/^Login: //g' | sed 's/[[:space:]]*Name:.*$//g' | tr -d '\n'
-}
